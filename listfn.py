d156b8a276d915a2a24325b82f7884dd44dec47d
asahere <arshas.bca2020@gmail.com>
2023-07-14 14:13:24 +0530

    Initial commit

--- listfn.py
+++ listfn.py
@@ -1,5 +1,20 @@
 l=[]
 print("Operations: \n1.Create a list \n2.Insert an element \n3.Delete an element \n4.Sort \n5.Exit")
+def create():
+    l=[]
+    n=int(input("enter a number"))
+    l.append(n)
+print(create)
+def insert():
+    
+print(insert)
+def delete(a,b):
+    A=a
+    B=b
+print(delete)  
+    A=a
+    B=b
+print(sort)
 ch=int(input("Enter the choice:"))
 while True:
     if ch==1:
