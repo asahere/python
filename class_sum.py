719b57d27cb467e742a1550c9ba52acb478f8a41
asahere <arshas.bca2020@gmail.com>
2023-07-31 16:02:47 +0530

    asd

---
+++ class_sum.py
@@ -0,0 +1,33 @@
+class calc:
+    def __init__(self,a,b):
+        self.num1=a
+        self.num2=b
+    def add(self):
+        self.sum=self.num1+self.num2
+        return self.sum
+    def sub(self):
+        self.min=self.num1-self.num2
+        return self.min
+    def mul(self):
+        self.multi=self.num1*self.num2
+        return self.multi
+    def div(self):
+        self.division=self.num1/self.num2
+        return self.division
+n=int(input("Enter a choice:"))
+if(n==0):
+    add1=calc(20,10)
+    print("Sum=",add1.add())
+elif(n==1):
+    sub1=calc(20,10)
+    print("Substraction=",sub1.sub())
+elif(n==2):
+    mul1=calc(20,10)
+    print("Multiplication=",mul1.mul())
+elif(n==3):
+    div1=calc(20,10)
+    print("Division=",div1.div())
+else:
+    print("Wrong choice vro")
+
+
