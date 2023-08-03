d19720ce94607a7df5bdc4cb3c67090eff51659e
asahere <arshas.bca2020@gmail.com>
2023-08-03 15:52:14 +0530

    asd

---
+++ time_add.py
@@ -0,0 +1,15 @@
+class time:
+    def __init__(self,hour,minute):
+        self.hour=hour
+        self.minute=minute
+        self.total=self.hour*60+self.minute
+    def addtime(self,obj2):
+        hrs=self.total//60
+        mts=self.total%60
+        print("Total time=",hrs:mts)
+    def displayTime(self):
+        print("Total time=",self.ntime)
+    def displayMinute(self):
+
+    
+    
