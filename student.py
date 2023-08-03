ae96f009a3ef74aab8bb2831c5738115392659da
asahere <arshas.bca2020@gmail.com>
2023-08-03 15:11:30 +0530

    asd

--- student.py
+++ student.py
@@ -1,12 +1,24 @@
 class student():
-    def __init__(self,name,age,rollnumber,mark):
+    def __init__(self,name,rollno):
         self.name=name
-        self.rollnumber=rollnumber 
-        self.age=age
-        self.mark=mark
+        self.rollno=rollno
     def display(self):
         print("Name=",self.name)
         print("Age=",self.age)
-        print("Roll Number=",self.rollnumber)
+        print("Roll Number=",self.rollno)
         print("Mark=",self.mark)
-    def student_age(self):
+    def set_age(self,age):
+        self.age=age
+    def set_marks(self,mark):
+        self.mark=mark
+name=input("Enter student name:")
+rollno=int(input("Enter the Roll Number:"))
+stud1=student(name,rollno)
+age=int(input("Enter the age:"))
+marks=float(input("Enter the total marks obtained:"))
+stud1.set_age(age)
+stud1.set_marks(marks)
+stud1.display()
+
+
+
