ef034c450c682552221719d3da7879a1c34332fe
RINSHIDKT <rinshidkt6@gmail.com>
2023-09-04 15:58:33 +0530

    asd

---
+++ tk1.py
@@ -0,0 +1,7 @@
+from tkinter import*
+root=Tk()
+Label(root,text="Red Sun",bg="red",fg="white").pack()
+Label(root,text="Green grass",bg="green",fg="black").pack()
+Label(root,text="Blue Sky",bg="blue",fg="white").pack()
+
+mainloop()
---
+++ tk5.py
@@ -0,0 +1,7 @@
+from tkinter import*
+root=Tk()
+w=Label(root,text="Green Grass",bg="green",fg="black")
+w.pack(ipadx=10)
+w=Label(root,text="Blue Sky",bg="blue",fg="White")
+w.pack(ipady=10)
+mainloop()
