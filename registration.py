dab7256c2fcb1b74beec68ba2a2b700706ac9b5a
asahere <arshas.bca2020@gmail.com>
2023-09-11 03:12:47 -0700

    asd

--- registration.py
+++ registration.py
@@ -2,44 +2,59 @@ import sqlite3
 from tkinter import messagebox                                 
 from tkinter import*
 con=sqlite3.connect("data.db")
-def check_user():
-    user=name.get()
-    passwd=passwd.get()
-    print(user,passwrd)
-    cursor.execute(
-        "Select * from login where username? and password?",(user,passwd)
-    )
-    res=cursor.fetchone()
-    if res:
-        messagebox.showinfo("Success", "Login Success")
-    else:
-        messagebox.showerror("Error","Login Failed but you are now registered as a new user")
-        cursor.execute(
-            "insert into login values(?,?)",(user,passwd)
-        )
-window=Tk()
-window.title("Login")
-window.minsize(200,200)
-name=StringVar()
-passwd=StringVar()
-lb1=Label(window,Text="Username",fg="red").grid(row=0,coloumn=1)
-en=Entry(window,highlightcolor="Orange",highlightthickness=20,textvariable=name).grid(row=1,column=2)
+#con.execute("CREATE TABLE register(username varchar(20),password varchar(20))")
+def r():
+    w1=Tk()
+    def db():
+        u=e.get()
+        p=e2.get()
+        con.execute("INSERT INTO register values(?,?)",(u,p))
+        con.commit()
+        messagebox.showinfo("register","success")
+        w1.destroy()
+    l1=Label(w1,text="username")
+    e=Entry(w1,width=20)
+    l2=Label(w1,text="password")
+    e2=Entry(w1,width=20)
+    l1.grid()
+    e.grid()
+    l2.grid()
+    e2.grid()
+    reg2=Button(w1,text="create",command=db)
+    reg2.grid(row=3,column=1)
+    mainloop()
 
-button=Button(
-    window,
-    text="Register"
-    bg="pink"
-    fg="black"
-    activebackground="purple",
-    command=check_user,
-).grid(row=2,column=1)
-button=Button(
-    window,
-    text="login",
-    bg="pink",
-    fg="black",
-    activebackground="purple",
-    command=check_user,
-).grid(row=2,column=2)
-con.commit()
-window.mainloop()
+def l():
+    w2=Tk()
+    def f():
+        u=e.get()
+        p=e2.get()
+        k=con.execute("SELECT * FROM register where username=? and password=?",(u,p))
+        for i in k:
+            messagebox.showinfo("login","success")
+            break
+        else:
+            messagebox.showinfo("login","invalid")
+        w2.destroy()
+    l1=Label(w2,text="username")
+    e=Entry(w2,width=20)
+    l2=Label(w2,text="password")
+    e2=Entry(w2,width=20)
+    l1.grid()
+    e.grid()
+    l2.grid()
+    e2.grid()
+    lg2=Button(w2,text="Login",command=f)
+    lg2.grid()
+    mainloop()
+
+w=Tk()
+reg=Button(w,text="Register",command=r)
+lg=Button(w,text="Login",command=l)
+reg.grid()
+lg.grid()
+mainloop()
+
+
+
+    
